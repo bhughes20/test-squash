c127d900b5875e3212d9705ea39764c7c96a012d
bhughes20 <74008833+bhughes20@users.noreply.github.com>
2021-10-25 10:50:39 +0000

    JIRA-bugfix-007
     - Change the endpoint
     - Change the message
    
    Create a test endpoint
    
    JIRA-feature-123-endpoint

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -3,6 +3,7 @@ package com.example.gitdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,9 +15,14 @@ public class GitDemoApplication {
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/hello-spring")
     String greet() {
-        return "Hello, World!";
+        return "Hello, Spring!";
+    }
+
+    @PostMapping("/test")
+    String test(){
+        return "test";
     }
 
 }
